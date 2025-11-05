003b8d8d65dabc1a86441f4923dae553cf47ae36
martinpanov <martin.panov98@gmail.com>
2025-11-05 20:21:49 +0200

    fix: [WA-1] Remove problematic package

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
-import purgecss from 'vite-plugin-purgecss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    purgecss({
-      content: ['./src/**/*.{ts,tsx,html}'],
-      safelist: {
-        standard: [/^toast/],
-        deep: [],
-        greedy: []
-      }
-    })
-  ]
+  plugins: [react()]
 });
